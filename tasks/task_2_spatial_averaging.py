2e3799ad4336a8d2d6b755b13cf99db9e4d7fe54
githmi <githmisundarasekara@gmail.com>
2025-06-17 19:30:08 +0530

    refactor: remove __main__ guard for simplicity in standalone script

--- tasks/task_2_spatial_averaging.py
+++ tasks/task_2_spatial_averaging.py
@@ -1,23 +1,22 @@
 import cv2
 import os
 
-if __name__ == "__main__":
-    os.makedirs("../results", exist_ok=True)
-    img = cv2.imread('../images/task2.jpg', cv2.IMREAD_GRAYSCALE)
-    if img is None:
-        print("Image not found!")
-        exit(1)
-    
-    blur_3x3 = cv2.blur(img, (3, 3))
-    output_filename = f"../results/task2_blur_3x3.jpg"
-    cv2.imwrite(output_filename, blur_3x3)
+os.makedirs("../results", exist_ok=True)
+img = cv2.imread('../images/task2.jpg', cv2.IMREAD_GRAYSCALE)
+if img is None:
+    print("Image not found!")
+    exit(1)
 
-    blur_10x10 = cv2.blur(img, (10, 10))
-    output_filename = f"../results/task2_blur_10x10.jpg"
-    cv2.imwrite(output_filename, blur_10x10)
+blur_3x3 = cv2.blur(img, (3, 3))
+output_filename = f"../results/task2_blur_3x3.jpg"
+cv2.imwrite(output_filename, blur_3x3)
 
-    blur_20x20 = cv2.blur(img, (20, 20))
-    output_filename = f"../results/task2_blur_20x20.jpg"
-    cv2.imwrite(output_filename, blur_20x20)
+blur_10x10 = cv2.blur(img, (10, 10))
+output_filename = f"../results/task2_blur_10x10.jpg"
+cv2.imwrite(output_filename, blur_10x10)
+
+blur_20x20 = cv2.blur(img, (20, 20))
+output_filename = f"../results/task2_blur_20x20.jpg"
+cv2.imwrite(output_filename, blur_20x20)
 
 
