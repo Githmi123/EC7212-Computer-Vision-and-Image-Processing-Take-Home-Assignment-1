55ffc51fa771fa068ba0fcb2e763f7a7171de71e
githmi <githmisundarasekara@gmail.com>
2025-06-17 19:17:41 +0530

    feat: spatial 3x3 average of image pixels

--- tasks/task_2_spatial_averaging.py
+++ tasks/task_2_spatial_averaging.py
@@ -1,3 +1,15 @@
 import cv2
+import os
 
-img = cv2.imread('../images/task2.jpg', cv2.IMREAD_GRAYSCALE)
+if __name__ == "__main__":
+    os.makedirs("../results", exist_ok=True)
+    img = cv2.imread('../images/task2.jpg', cv2.IMREAD_GRAYSCALE)
+    if img is None:
+        print("Image not found!")
+        exit(1)
+    
+    blur_3x3 = cv2.blur(img, (3, 3))
+    output_filename = f"../results/task2_blur_3x3.jpg"
+    cv2.imwrite(filename, blur_3x3)
+
+
