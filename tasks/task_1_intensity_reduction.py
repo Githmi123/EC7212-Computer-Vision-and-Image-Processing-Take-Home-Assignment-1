2706df61d96056bb98ce86ef544675f8604ae606
githmi <githmisundarasekara@gmail.com>
2025-06-17 06:44:13 +0530

    feat: develop a function to reduce intensity levels & and inside it add logic to validate user input

--- tasks/task_1_intensity_reduction.py
+++ tasks/task_1_intensity_reduction.py
@@ -1,5 +1,14 @@
 import cv2
 
+def reduce_intensity_levels(img, levels):
+    if (levels < 2 or levels > 256):
+        raise ValueError("Levels must be between 2 and 256 inclusive.")
+
+    desired_levels = 2 ** i for i in range(1, 9)
+
+    if levels not in desired_levels:
+        raise ValueError("Levels must be one of the following: 2, 4, 8, 16, 32, 64, 128, 256")
+
 if __name__ == "__main__":
     img = cv2.imread("../images/task1.jpg", cv2.IMREAD_GRAYSCALE)
     
@@ -10,4 +19,5 @@ if __name__ == "__main__":
     print("Original Image Shape: ", img.shape)
     
     levels = int(input("Enter the number of intensity levels (2-256, in integer poers of 2): "))
-    
+
+
