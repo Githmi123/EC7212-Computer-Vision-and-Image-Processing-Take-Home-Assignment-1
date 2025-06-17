a2fadf717fcba1686d8ee8fc5b8c668ca6e067a7
githmi <githmisundarasekara@gmail.com>
2025-06-17 19:22:57 +0530

    bugfix: correct filename variable to output_filename

--- tasks/task_2_spatial_averaging.py
+++ tasks/task_2_spatial_averaging.py
@@ -10,14 +10,14 @@ if __name__ == "__main__":
     
     blur_3x3 = cv2.blur(img, (3, 3))
     output_filename = f"../results/task2_blur_3x3.jpg"
-    cv2.imwrite(filename, blur_3x3)
+    cv2.imwrite(output_filename, blur_3x3)
 
     blur_10x10 = cv2.blur(img, (10, 10))
     output_filename = f"../results/task2_blur_10x10.jpg"
-    cv2.imwrite(filename, blur_10x10)
+    cv2.imwrite(output_filename, blur_10x10)
 
     blur_20x20 = cv2.blur(img, (20, 20))
     output_filename = f"../results/task2_blur_20x20.jpg"
-    cv2.imwrite(filename, blur_20x20)
+    cv2.imwrite(output_filename, blur_20x20)
 
 
